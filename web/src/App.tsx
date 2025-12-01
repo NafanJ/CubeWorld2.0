e323bfb557c0da3e217a8ee500cc91be89a6a670
Nathan <nathanjoeldev@gmail.com>
2025-12-01 17:34:02 +0000

    Lock viewport

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,12 +9,12 @@ export function App() {
       style={{ backgroundImage: 'url(/backgroundImage.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
       {/* Room Grid */}
-      <div className="w-full lg:w-3/5 flex-1 min-h-0 flex items-center justify-center p-4">
+      <div className="w-full lg:w-3/5 flex-1 min-h-0 flex items-center justify-center p-4 overflow-auto">
         <PixelRoomGrid />
       </div>
 
       {/* Chat Panel - side by side on large screens, stacked below on mobile */}
-      <div className="w-full lg:w-2/5 lg:h-full h-[35vh] min-h-0 flex-shrink-0">
+      <div className="w-full lg:w-2/5 lg:h-full h-[35vh] flex-shrink-0 overflow-hidden">
         <ChatPanel />
       </div>
     </div>
